d267c593d016e613237b3d61e74e4ccff554dd36
João Vitor Witt <joaovitorwitt2004@gmail.com>
2023-06-26 16:16:40 -0300

    Create CrowdFunding.sol

---
+++ web3/contracts/CrowdFunding.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+contract CrowdFunding {
+
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint target;
+        uint deadline;
+        uint amountCollected;
+        string image;
+        address[] donators;
+        uint[] donations;
+    }
+
+    mapping (uint=> Campaign) public campaings;
+
+    uint public numberOfCampaings = 0;
+
+
+    function createCampaing() {}
+}
